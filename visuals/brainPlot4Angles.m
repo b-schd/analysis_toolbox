ccda8841d5142d657e4452543856bf0469fa1541
Brittany Scheid <bscheid@seas.upenn.edu>
2022-11-11 19:52:12 -0500

    update to allow for view selection and compatibility with text functionality

--- visuals/brainPlot4Angles.m
+++ visuals/brainPlot4Angles.m
@@ -1,8 +1,17 @@
 function h = brainPlot4Angles(coords, labels, options)
+% h = brainPlot4Angles(coords, labels, options)
+%
+% options:
+%   color = 'blue'
+%   size = 100 
+%   MNI = false
+%   surfaceAlpha = 0.2
+%
 
 arguments
     coords
     labels = []
+    options.views = [1:4]
     options.color = 'blue'; 
     options.size = 100
     options.MNI= false
@@ -11,16 +20,24 @@ end
 
 v_idx = [[90,0]; [270, 0]; [180,0]; [180, 90]];
 
-for i = (1:4)
+if length(options.views) == 1
+    m = 1; n = 1; 
+elseif length(options.views) == 2
+    m=1; n = 2; 
+else
+    m = 2; n= 2; 
+end
 
-    subplot(2,2,i); hold on
+for i = options.views
+
+    subplot(m,n,i); hold on
     if options.color
         scatter3(coords(:,1), coords(:,2), coords(:,3), options.size, options.color, 'filled')
     else
         scatter3(coords(:,1), coords(:,2), coords(:,3), options.size, 'filled')
     end
     if ~isempty(labels)
-        textscatter3(coords(:,1), coords(:,2), coords(:,3), labels, 'fontsize', 10, 'TextDensityPercentage', 100)
+        text(coords(:,1), coords(:,2), coords(:,3), labels, 'fontsize', 10)
     end
       
     if options.MNI
